77726d5a1abce155a3e5686f9fab6575d809d5d5
dngrey <qwerty.qq185@gmail.com>
2018-11-04 16:08:09 +0200

    about

--- wp-content/themes/yandy-master/about.php
+++ wp-content/themes/yandy-master/about.php
@@ -5,16 +5,19 @@ Template Post Type: post, page, product
 */
 
 get_header();
-?> 
+?>
   <section class="blog-banner container">
-  	   	 	<?php 
+  	   	 	<?php
 
 $prev = get_field('image_previe');
 $images = get_field('image_desc2');
 $image = get_field('image_desc');
 $images = get_field('image_desc2');
-$imageb = get_field('image_desc_bottom');
-$imagesb = get_field('image_desc_bottom2');
+$imageb1 = get_field('image_desc_bottom1');
+$imageb2 = get_field('image_desc_bottom2');
+$imageb3 = get_field('image_desc_bottom3');
+$imageb4 = get_field('image_desc_bottom4');
+$imageb5 = get_field('image_desc_bottom5');
 
 
 ?>
@@ -22,63 +25,56 @@ $imagesb = get_field('image_desc_bottom2');
 
            <img src="<?php echo $prev['url']; ?>" alt="<?php echo $prev['alt']; ?>"  class="img-fluid">
   </section>
+
   <section class="blog-header">
     <div class="container">
-      <div  class="coming">
-        <div class="top-h1 "> <?php the_field('name'); ?></div>
-         <?php
-if ( have_posts() ) : // если имеются записи в блоге.
+      <div class="snow-box head-snow-box blog-text">
+        <h1 class="top-h1-2 subtitle-h1"><span><?php the_field('name'); ?></span></h1>
 
-  while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
-?>
-        <h4><span class="name-event">Y&Y CURRENT EVENTS</span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<span class="data-event"><?php the_date(); ?></span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<span class="writer-event"><?php the_author(); ?></span></h4>
-   
+       <?php
+          if ( have_posts() ) : // если имеются записи в блоге.
 
+            while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
+          ?>
+            <div class="event-title-row"><span class="event-title name-event">Y&Y CURRENT EVENTS</span><span class="text-divider">|</span><span class="event-title data-event"><?php the_date(); ?></span><span class="text-divider">|</span><span class="event-title writer-event"><?php the_author(); ?></span></div>
 
-<?php
-the_content();
-			
- wp_reset_postdata();
+          <?php
+          the_content();
 
-  endwhile;  // завершаем цикл.
-endif;
-/* Сбрасываем настройки цикла. Если ниже по коду будет идти еще один цикл, чтобы не было сбоя. */
-wp_reset_query();                
-?>
+           wp_reset_postdata();
+
+            endwhile;  // завершаем цикл.
+          endif;
+          /* Сбрасываем настройки цикла. Если ниже по коду будет идти еще один цикл, чтобы не было сбоя. */
+          wp_reset_query();
+        ?>
       </div>
     </div>
   </section>
+
   <section class="blog-info">
-    <div class="container">
-      <div class="blog-images">
-     		
+      <div class="container">
 
-
-        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-fluid">
-         <img src="<?php echo $images['url']; ?>" alt="<?php echo $images['alt']; ?>" class="img-fluid">
-       
-      </div>
-      <div class="blog-text">
- 
-        <?php the_field('text_desc'); ?>
+        <div class="about-blog-images">
+          <span class="about-image-item">
+            <img src="<?php echo $imageb1['url']; ?>" alt="<?php echo $imageb1['alt']; ?>">
+          </span>
+          <span class="about-image-item">
+            <img src="<?php echo $imageb2['url']; ?>" alt="<?php echo $imageb2['alt']; ?>">
+          </span>
+          <span class="about-image-item">
+            <img src="<?php echo $imageb3['url']; ?>" alt="<?php echo $imageb3['alt']; ?>">
+          </span>
+          <span class="about-image-item">
+            <img src="<?php echo $imageb4['url']; ?>" alt="<?php echo $imageb4['alt']; ?>">
+          </span>
+          <span class="about-image-item">
+            <img src="<?php echo $imageb5['url']; ?>" alt="<?php echo $imageb5['alt']; ?>">
+          </span>
         </div>
+
       </div>
     </div>
   </section>
-  <section class="blog-info">
-    <div class="container">
-      <div class="blog-images">
-   
 
-        <img src="<?php echo $imageb['url']; ?>" alt="<?php echo $imageb['alt']; ?>" class="img-fluid">
-         <img src="<?php echo $imagesb['url']; ?>" alt="<?php echo $imagesb['alt']; ?>" class="img-fluid">
-        
-
-      </div>
-      <div class="blog-text">
-         <?php the_field('text_desc_bottom2'); ?>
-        </div>
-      </div>
-    </div>
-  </section>
   <?php wp_footer(); ?>
